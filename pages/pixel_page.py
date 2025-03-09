871b87adb135192c1bdf050c6f777b638989b9e6
Shlomi gross <shlomi10@gmail.com>
2025-03-09 23:16:21 +0200

    update

--- pages/pixel_page.py
+++ pages/pixel_page.py
@@ -1,4 +1,5 @@
 import allure
+from playwright.sync_api import expect
 
 from pages.basePage import BasePage
 
@@ -12,8 +13,9 @@ This file contains the pixel 9 pro xl page
 class PixelPage(BasePage):
     def __init__(self, page):
         super().__init__(page)
-        self.google_pixel_title = page.locator(".specs-phone-name-title")
+        self.__google_pixel_title = page.locator(".specs-phone-name-title")
 
     @allure.step("validate pixel 9 pro xl in title")
     def get_title_of_device(self) -> str:
-        return self.get_text(self.google_pixel_title)
+        expect(self.__google_pixel_title).to_be_visible(timeout=5000)
+        return self.get_text(self.__google_pixel_title)
